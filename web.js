aefd029cdd415ba1542bf9199a59e8db3fbff4bf
Katrin Shechtman <katrin.shechtman@gmail.com>
2014-11-23 01:15:48 -0500

    statuc dir should be dist (?)

--- web.js
+++ web.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
  
 app.use(express.logger('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
